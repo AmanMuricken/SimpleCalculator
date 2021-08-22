3cc27c29e0d3aab28555b62f2315db5d5c109980
AmanMuricken <89331271+AmanMuricken@users.noreply.github.com>
2021-08-22 10:57:46 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,14 @@
+a = 60
+b = 40
+sum = a+b
+sub = a-b
+mult = a*b
+div = a/b
+rem = a%b
+
+print("simple calculator")
+print("sum of a and b",sum)
+print("subtraction of a and b",sub)
+print("multiplication of a and b",mult)
+print("division of a and b",div)
+print("remainder of a and b",rem)
